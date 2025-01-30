e417d283a22f81b82f3c63761b41dcea13222095
Alexander Soklev <alex@makepath.com>
2025-01-30 22:01:35 +0200

    Testing why test are failing (wip)

--- pyrte_rrtmgp/tests/rrtmgp_compute_Planck_source_test/compute_Planck_source_test.py
+++ pyrte_rrtmgp/tests/rrtmgp_compute_Planck_source_test/compute_Planck_source_test.py
@@ -3,32 +3,99 @@ import json
 import numpy as np
 import pyrte_rrtmgp.pyrte_rrtmgp as py
 import os
+import copy as cp
 import zipfile
 
-def test_rrtmgp_compute_Planck_source(request):
+def load_data(path):
 
-    path = os.path.dirname(request.path)
+    # Read dimensions
+    with open(os.path.join(path, "inputs-dim.bin"), "rb") as f:
+        dims = np.fromfile(f, dtype=np.int32, count=9)
+    ncol, nlay, nbnd, ngpt, nflav, neta, npres, ntemp, nPlanckTemp = dims
 
-    input_data = None
-    output_data = None
-    with zipfile.ZipFile(f'{path}/compute_Planck_source_test_data.zip') as myzip:
-        with myzip.open('compute_Planck_source_input.json') as input_data_file:
-            input_data = json.load(input_data_file)
-        with myzip.open('compute_Planck_source_output.json') as output_data_file:
-            output_data = json.load(output_data_file)
+    print("Dimensions:", ncol, nlay, nbnd, ngpt, nflav, neta, npres, ntemp, nPlanckTemp)
 
-    for key in input_data:
-        values = input_data[key]
-        if isinstance(values, list):
-            if isinstance(values[0], int):
-                values = np.array(values, dtype=np.int32)
-            else:
-                values = np.array(values, dtype=np.float64)
-            input_data[key] = values
+    # Read integer data
+    data = {
+        "ncol": ncol,
+        "nlay": nlay,
+        "nbnd": nbnd,
+        "ngpt": ngpt,
+        "nflav": nflav,
+        "neta": neta,
+        "npres": npres,
+        "ntemp": ntemp,
+        "nPlanckTemp": nPlanckTemp,
+        "tlay": np.fromfile(os.path.join(path, "inputs-tlay.bin"), dtype=np.float64, count=ncol * nlay),#.reshape((ncol, nlay), order='C'),
+        "tlev": np.fromfile(os.path.join(path, "inputs-tlev.bin"), dtype=np.float64, count=ncol * (nlay + 1)),#.reshape((ncol, nlay + 1), order='C'),
+        "tsfc": np.fromfile(os.path.join(path, "inputs-tsfc.bin"), dtype=np.float64, count=ncol),
+        "sfc_lay": np.fromfile(os.path.join(path, "inputs-sfc_lay.bin"), dtype=np.int32, count=1)[0],
+        "fmajor": np.fromfile(os.path.join(path, "inputs-fmajor.bin"), dtype=np.float64, count=2 * 2 * 2 * ncol * nlay * nflav),#.reshape((2, 2, 2, ncol, nlay, nflav), order='C'),
+        "jeta": np.fromfile(os.path.join(path, "inputs-jeta.bin"), dtype=np.int32, count=2 * ncol * nlay * nflav),#.reshape((2, ncol, nlay, nflav), order='C'),
+        "tropo": np.fromfile(os.path.join(path, "inputs-tropo.bin"), dtype=np.bool_, count=ncol * nlay),#.reshape((ncol, nlay), order='C'),
+        "jtemp": np.fromfile(os.path.join(path, "inputs-jtemp.bin"), dtype=np.int32, count=ncol * nlay),#.reshape((ncol, nlay), order='C'),
+        "jpress": np.fromfile(os.path.join(path, "inputs-jpress.bin"), dtype=np.int32, count=ncol * nlay),#.reshape((ncol, nlay), order='C'),
+        "gpoint_bands": np.fromfile(os.path.join(path, "inputs-gpoint_bands.bin"), dtype=np.int32, count=ngpt),
+        "band_lims_gpt": np.fromfile(os.path.join(path, "inputs-band_lims_gpt.bin"), dtype=np.int32, count=2 * nbnd),#.reshape((2, nbnd), order='C'),
+        "pfracin": np.fromfile(os.path.join(path, "inputs-pfracin.bin"), dtype=np.float64, count=ntemp * neta * (npres + 1) * ngpt),#.reshape((ntemp, neta, npres + 1, ngpt), order='C'),
+        "temp_ref_min": np.fromfile(os.path.join(path, "inputs-temp_ref_min.bin"), dtype=np.float64, count=1)[0],
+        "totplnk_delta": np.fromfile(os.path.join(path, "inputs-totplnk_delta.bin"), dtype=np.float64, count=1)[0],
+        "totplnk": np.fromfile(os.path.join(path, "inputs-totplnk.bin"), dtype=np.float64, count=nPlanckTemp * nbnd),#.reshape((nPlanckTemp, nbnd), order='C'),
+        "gpoint_flavor": np.fromfile(os.path.join(path, "inputs-gpoint_flavor.bin"), dtype=np.int32, count=2 * ngpt),#.reshape((2, ngpt), order='C'),
+        "sfc_src": np.fromfile(os.path.join(path, "inputs-sfc_src.bin"), dtype=np.float64, count=ncol * ngpt),#.reshape((ncol, ngpt), order='C'),
+        "lay_src": np.fromfile(os.path.join(path, "inputs-lay_src.bin"), dtype=np.float64, count=ncol * nlay * ngpt),#.reshape((ncol, nlay, ngpt), order='C'),
+        "lev_src": np.fromfile(os.path.join(path, "inputs-lev_src.bin"), dtype=np.float64, count=ncol * (nlay + 1) * ngpt),#.reshape((ncol, nlay + 1, ngpt), order='C'),
+        "sfc_source_Jac": np.fromfile(os.path.join(path, "inputs-sfc_source_Jac.bin"), dtype=np.float64, count=ncol * ngpt),#.reshape((ncol, ngpt), order='C')
+    }
 
+    return data
+
+def test_rrtmgp_compute_Planck_source():
+    """
+    Test the Fortran function `rrtmgp_compute_Planck_source` by comparing its outputs
+    against a reference.
+    """
+    output_keys = [
+        "sfc_src",
+        "lay_src",
+        "lev_src",
+        "sfc_source_Jac"
+    ]
+
+    path = "/data/vkm/code/makepath/rte-rrtmgp/build/examples/rfmip-clear-sky/"
+    pathRef = "/data/vkm/code/makepath/rte-rrtmgp/tmp"
+
+    # Load input data
+    input_data  = load_data(path)
+    output_data = load_data(pathRef)
+
+    # Prepare arguments for Fortran function
     args = list(input_data.values())
 
+    # Call the Fortran function
     py.rrtmgp_compute_Planck_source(*args)
 
-    for key in output_data:
-        assert(np.allclose(output_data[key] - input_data[key], 0.))
+    # Validate outputs
+    for key in output_keys:
+        computed  = input_data[key]
+        reference = output_data[key]
+
+        threshold = 0.0#7.e-4
+
+        diff  = np.abs(reference - computed)
+        mask  = diff > threshold
+        count = np.sum(mask)
+
+        if count > 0:
+            indices = np.argwhere(mask)
+            for idx in indices:
+                ref_val = reference[tuple(idx)]
+                computed_val = computed[tuple(idx)]
+                diff_val = diff[tuple(idx)]
+                # print(f"#{idx}\t{ref_val}\t| {computed_val}\t>> {diff_val}")
+
+        print(f"{key} shape: {computed.shape}\tDiff count: {count}\tMax diff: {np.max(diff)}")
+
+        # assert np.allclose(ref, computed, 0.0), f"Mismatch in {key}: {np.max(np.abs(ref - computed))} difference"
+
+test_rrtmgp_compute_Planck_source()
--- pyrte_rrtmgp/tests/rrtmgp_compute_tau_absorption_test/compute_tau_absorption_test.py
+++ pyrte_rrtmgp/tests/rrtmgp_compute_tau_absorption_test/compute_tau_absorption_test.py
@@ -1,33 +1,186 @@
-#!/usr/bin/env python3
-import json
-import numpy as np
-import pyrte_rrtmgp.pyrte_rrtmgp as py
-import os
-import zipfile
+# #!/usr/bin/env python3
+# import json
+# import numpy as np
+# import pyrte_rrtmgp.pyrte_rrtmgp as py
+# import os
+# import copy as cp
 
-def test_rrtmgp_compute_tau_absorption(request):
-    path = os.path.dirname(request.path)
+# def load_data(path):
 
-    input_data = None
-    output_data = None
-    with zipfile.ZipFile(f'{path}/compute_tau_absorption_test_data.zip') as myzip:
-        with myzip.open('compute_tau_absorption_input.json') as input_data_file:
-            input_data = json.load(input_data_file)
-        with myzip.open('compute_tau_absorption_output.json') as output_data_file:
-            output_data = json.load(output_data_file)
+#     # Read dimensions
+#     with open(os.path.join(path, "inputs-dims.bin"), "rb") as f:
+#         dims = np.fromfile(f, dtype=np.int32, count=9)
+#     ncol, nlay, nbnd, ngpt, ngas, nflav, neta, npres, ntemp = dims
 
-    for key in input_data:
-        values = input_data[key]
-        if isinstance(values, list):
-            if isinstance(values[0], int):
-                values = np.array(values, dtype=np.int32)
-            else:
-                values = np.array(values, dtype=np.float64)
-            input_data[key] = values
+#     with open(os.path.join(path, "inputs-nminors.bin"), "rb") as f:
+#         nminors = np.fromfile(f, dtype=np.int32, count=4)
+#     nminorlower, nminorklower, nminorupper, nminorkupper = nminors
 
-    args = list(input_data.values())
+#     print("Dimensions:", ncol, nlay, nbnd, ngpt, ngas, nflav, neta, npres, ntemp)
+#     print("NMinors:", nminorlower, nminorklower, nminorupper, nminorkupper)
 
-    py.rrtmgp_compute_tau_absorption(*args)
+#     # Read integer data
+#     data = {
+#         "ncol": ncol,
+#         "nlay": nlay,
+#         "nbnd": nbnd,
+#         "ngpt": ngpt,
+#         "ngas": ngas,
+#         "nflav": nflav,
+#         "neta": neta,
+#         "npres": npres,
+#         "ntemp": ntemp,
+#         "nminorlower": nminorlower,
+#         "nminorklower": nminorklower,
+#         "nminorupper": nminorupper,
+#         "nminorkupper": nminorkupper,
+#         "idx_h2o": np.fromfile(os.path.join(path, "inputs-idx_h2o.bin"), dtype=np.int32, count=1)[0],
+#         "gpoint_flavor": np.fromfile(os.path.join(path, "inputs-gpoint_flavor.bin"), dtype=np.int32, count=2 * ngpt),
+#         "band_lims_gpt": np.fromfile(os.path.join(path, "inputs-band_lims_gpt.bin"), dtype=np.int32, count=2 * nbnd),
+#         "kmajor": np.fromfile(os.path.join(path, "inputs-kmajor.bin"), dtype=np.float64, count=ntemp * neta * (npres+1) * ngpt),
+#         "kminor_lower": np.fromfile(os.path.join(path, "inputs-kminor_lower.bin"), dtype=np.float64, count=ntemp * neta * nminorklower),
+#         "kminor_upper": np.fromfile(os.path.join(path, "inputs-kminor_upper.bin"), dtype=np.float64, count=ntemp * neta * nminorkupper),
+#         "minor_limits_gpt_lower": np.fromfile(os.path.join(path, "inputs-minor_limits_gpt_lower.bin"), dtype=np.int32, count=2 * nminorlower),
+#         "minor_limits_gpt_upper": np.fromfile(os.path.join(path, "inputs-minor_limits_gpt_upper.bin"), dtype=np.int32, count=2 * nminorupper),
+#         "minor_scales_with_density_lower": np.fromfile(os.path.join(path, "inputs-minor_scales_with_density_lower.bin"), dtype=np.bool_, count=nminorlower),
+#         "minor_scales_with_density_upper": np.fromfile(os.path.join(path, "inputs-minor_scales_with_density_upper.bin"), dtype=np.bool_, count=nminorupper),
+#         "scale_by_complement_lower": np.fromfile(os.path.join(path, "inputs-scale_by_complement_lower.bin"), dtype=np.bool_, count=nminorlower),
+#         "scale_by_complement_upper": np.fromfile(os.path.join(path, "inputs-scale_by_complement_upper.bin"), dtype=np.bool_, count=nminorupper),
+#         "idx_minor_lower": np.fromfile(os.path.join(path, "inputs-idx_minor_lower.bin"), dtype=np.int32, count=nminorlower),
+#         "idx_minor_upper": np.fromfile(os.path.join(path, "inputs-idx_minor_upper.bin"), dtype=np.int32, count=nminorupper),
+#         "idx_minor_scaling_lower": np.fromfile(os.path.join(path, "inputs-idx_minor_scaling_lower.bin"), dtype=np.int32, count=nminorlower),
+#         "idx_minor_scaling_upper": np.fromfile(os.path.join(path, "inputs-idx_minor_scaling_upper.bin"), dtype=np.int32, count=nminorupper),
+#         "kminor_start_lower": np.fromfile(os.path.join(path, "inputs-kminor_start_lower.bin"), dtype=np.int32, count=nminorlower),
+#         "kminor_start_upper": np.fromfile(os.path.join(path, "inputs-kminor_start_upper.bin"), dtype=np.int32, count=nminorupper),
+#         "tropo": np.fromfile(os.path.join(path, "inputs-tropo.bin"), dtype=np.bool_, count=ncol * nlay),
+#         "col_mix": np.fromfile(os.path.join(path, "inputs-col_mix.bin"), dtype=np.float64, count=2 * ncol * nlay * nflav),
+#         "fmajor": np.fromfile(os.path.join(path, "inputs-fmajor.bin"), dtype=np.float64, count=2 * 2 * 2 * ncol * nlay * nflav),
+#         "fminor": np.fromfile(os.path.join(path, "inputs-fminor.bin"), dtype=np.float64, count=2 * 2 * ncol * nlay *nflav),
+#         "play": np.fromfile(os.path.join(path, "inputs-play.bin"), dtype=np.float64, count=ncol * nlay),
+#         "tlay": np.fromfile(os.path.join(path, "inputs-tlay.bin"), dtype=np.float64, count=ncol * nlay),
+#         "col_gas": np.fromfile(os.path.join(path, "inputs-col_gas.bin"), dtype=np.float64, count=ncol * nlay * (ngas + 1)),
+#         "jeta": np.fromfile(os.path.join(path, "inputs-jeta.bin"), dtype=np.int32, count=2 * ncol * nlay * nflav),
+#         "jtemp": np.fromfile(os.path.join(path, "inputs-jtemp.bin"), dtype=np.int32, count=ncol * nlay),
+#         "jpress": np.fromfile(os.path.join(path, "inputs-jpress.bin"), dtype=np.int32, count=ncol * nlay),
+#         "tau": np.fromfile(os.path.join(path, "inputs-tau.bin"), dtype=np.float64, count=ncol * nlay *ngpt)
+#     }
 
-    for key in output_data:
-        assert(np.allclose(output_data[key] - input_data[key], 0.))
+#     return data
+
+# def load_data1(path):
+#     # Read dimensions
+#     with open(os.path.join(path, "inputs-dims.bin"), "rb") as f:
+#         dims = np.fromfile(f, dtype=np.int32, count=9)
+#     ncol, nlay, nbnd, ngpt, ngas, nflav, neta, npres, ntemp = dims
+
+#     with open(os.path.join(path, "inputs-nminors.bin"), "rb") as f:
+#         nminors = np.fromfile(f, dtype=np.int32, count=4)
+#     nminorlower, nminorklower, nminorupper, nminorkupper = nminors
+
+#     print("Dimensions:", ncol, nlay, nbnd, ngpt, ngas, nflav, neta, npres, ntemp)
+#     print("NMinors:", nminorlower, nminorklower, nminorupper, nminorkupper)
+
+#     def read_binary(filename, dtype, shape):
+#         """Helper function to read binary file and reshape it."""
+#         count = np.prod(shape)
+#         data = np.fromfile(os.path.join(path, filename), dtype=dtype, count=count)
+
+#         if shape:
+#             return data.reshape(shape, order="C")  # Fortran order reshaping
+#         return data
+
+#     data = {
+#         "ncol": ncol,
+#         "nlay": nlay,
+#         "nbnd": nbnd,
+#         "ngpt": ngpt,
+#         "ngas": ngas,
+#         "nflav": nflav,
+#         "neta": neta,
+#         "npres": npres,
+#         "ntemp": ntemp,
+#         "nminorlower": nminorlower,
+#         "nminorklower": nminorklower,
+#         "nminorupper": nminorupper,
+#         "nminorkupper": nminorkupper,
+#         "idx_h2o": read_binary("inputs-idx_h2o.bin", np.int32, (1,)),
+#         "gpoint_flavor": read_binary("inputs-gpoint_flavor.bin", np.int32, (2, ngpt)),
+#         "band_lims_gpt": read_binary("inputs-band_lims_gpt.bin", np.int32, (2, nbnd)),
+#         "kmajor": read_binary("inputs-kmajor.bin", np.float64, (ntemp, neta, npres+1, ngpt)),
+#         "kminor_lower": read_binary("inputs-kminor_lower.bin", np.float64, (ntemp, neta, nminorklower)),
+#         "kminor_upper": read_binary("inputs-kminor_upper.bin", np.float64, (ntemp, neta, nminorkupper)),
+#         "minor_limits_gpt_lower": read_binary("inputs-minor_limits_gpt_lower.bin", np.int32, (2, nminorlower)),
+#         "minor_limits_gpt_upper": read_binary("inputs-minor_limits_gpt_upper.bin", np.int32, (2, nminorupper)),
+#         "minor_scales_with_density_lower": read_binary("inputs-minor_scales_with_density_lower.bin", np.bool_, (nminorlower,)),
+#         "minor_scales_with_density_upper": read_binary("inputs-minor_scales_with_density_upper.bin", np.bool_, (nminorupper,)),
+#         "scale_by_complement_lower": read_binary("inputs-scale_by_complement_lower.bin", np.bool_, (nminorlower,)),
+#         "scale_by_complement_upper": read_binary("inputs-scale_by_complement_upper.bin", np.bool_, (nminorupper,)),
+#         "idx_minor_lower": read_binary("inputs-idx_minor_lower.bin", np.int32, (nminorlower,)),
+#         "idx_minor_upper": read_binary("inputs-idx_minor_upper.bin", np.int32, (nminorupper,)),
+#         "idx_minor_scaling_lower": read_binary("inputs-idx_minor_scaling_lower.bin", np.int32, (nminorlower,)),
+#         "idx_minor_scaling_upper": read_binary("inputs-idx_minor_scaling_upper.bin", np.int32, (nminorupper,)),
+#         "kminor_start_lower": read_binary("inputs-kminor_start_lower.bin", np.int32, (nminorlower,)),
+#         "kminor_start_upper": read_binary("inputs-kminor_start_upper.bin", np.int32, (nminorupper,)),
+#         "tropo": read_binary("inputs-tropo.bin", np.bool_, (ncol, nlay)),
+#         "col_mix": read_binary("inputs-col_mix.bin", np.float64, (2, ncol, nlay, nflav)),
+#         "fmajor": read_binary("inputs-fmajor.bin", np.float64, (2, 2, 2, ncol, nlay, nflav)),
+#         "fminor": read_binary("inputs-fminor.bin", np.float64, (2, 2, ncol, nlay, nflav)),
+#         "play": read_binary("inputs-play.bin", np.float64, (ncol, nlay)),
+#         "tlay": read_binary("inputs-tlay.bin", np.float64, (ncol, nlay)),
+#         "col_gas": read_binary("inputs-col_gas.bin", np.float64, (ncol, nlay, ngas+1)),
+#         "jeta": read_binary("inputs-jeta.bin", np.int32, (2, ncol, nlay, nflav)),
+#         "jtemp": read_binary("inputs-jtemp.bin", np.int32, (ncol, nlay)),
+#         "jpress": read_binary("inputs-jpress.bin", np.int32, (ncol, nlay)),
+#         "tau": read_binary("inputs-tau.bin", np.float64, (ncol, nlay, ngpt))
+#     }
+
+#     return data
+
+
+# def test_rrtmgp_compute_tau_absorption():
+#     """
+#     Test the Fortran function `test_rrtmgp_compute_tau_absorption` by comparing its outputs
+#     against a reference.
+#     """
+#     output_keys = [
+#         "tau"
+#     ]
+
+#     # path = "/data/vkm/code/makepath/rte-rrtmgp/build/tests"
+#     path = "/data/vkm/code/makepath/rte-rrtmgp/build/examples/all-sky"
+
+#     # Load input data
+#     input_data = load_data(path)
+#     # # Copy input data to compare outputs
+#     output_data = cp.deepcopy(input_data)
+
+#     # Print keys of both dictionaries
+
+#     # Prepare arguments for Fortran function
+#     args = list(input_data.values())
+
+#     # Call the Fortran function
+#     py.rrtmgp_compute_tau_absorption(*args)
+
+#     # Validate outputs
+#     for key in output_keys:
+#         ref = output_data[key]
+#         computed = input_data[key]
+
+#         diff = np.abs(ref - computed)
+#         threshold = 0.1 #7.e-4
+#         mask = diff > threshold
+#         count = np.sum(mask)
+#         if count > 0:
+#             indices = np.argwhere(mask)
+#             for idx in indices:
+#                 ref_val = ref[tuple(idx)]
+#                 computed_val = computed[tuple(idx)]
+#                 diff_val = diff[tuple(idx)]
+#                 # print(f"#{idx}\t{ref_val}\t| {computed_val}\t>> {diff_val}")
+
+#         print(f"{key} shape: {computed.shape}\tDiff count: {count}\tMax diff: {np.max(diff)}")
+
+#         # assert np.allclose(ref, computed, 0.0), f"Mismatch in {key}: {np.max(np.abs(ref - computed))} difference"
+
+# test_rrtmgp_compute_tau_absorption()
